a12af329784551aaa3593abb57d8435e76f0dc04
arnemoos <arne.moos@tu-dortmund.de>
2018-02-08 14:41:44 +0100

    - integrated create_xml_annotation (to disable use -x flag)

--- main.py
+++ main.py
@@ -7,7 +7,75 @@ from http_requests import RequestHandler, ImageSetNotFoundError, ImageSetPermiss
 from utils import parse_image_id_from_url
 from png import read_label_chunk, write_label_chunk
 from protobuf import decode_data, encode_data
+import xml.etree.cElementTree as ET
 
+def save_annotations_to_xml(file_path, file_name):
+    label_data = decode_data(os.path.join(file_path, file_name), False)
+
+    xml_root = ET.Element("annotation")
+    ET.SubElement(xml_root, "filename").text = file_name
+
+    xml_size = ET.SubElement(xml_root, "size")
+    ET.SubElement(xml_size, "width").text = str(label_data.cameraInformation.resolution.width)
+    ET.SubElement(xml_size, "height").text = str(label_data.cameraInformation.resolution.height)
+    # TODO check for grayscale
+    ET.SubElement(xml_size, "depth").text = "3"
+
+    isRobotInImage = None
+    isBallInImage = None
+
+    # read all robots
+    for robot in label_data.robots:
+        xmin = str(robot.label.boundingBox.upperLeft.x)
+        ymin = str(robot.label.boundingBox.upperLeft.y)
+        xmax = str(robot.label.boundingBox.upperLeft.x)
+        ymax = str(robot.label.boundingBox.upperLeft.y)
+        if robot.label.boundingBox.upperLeft.x >= 0 and \
+                        robot.label.boundingBox.upperLeft.y >= 0 and \
+                        robot.label.boundingBox.upperLeft.x >= 0 and \
+                        robot.label.boundingBox.upperLeft.y >= 0:
+            isRobotInImage = True
+            xml_object = ET.SubElement(xml_root, "object")
+            ET.SubElement(xml_object, "name").text = "robot"
+            create_xml_bndbox(xml_object, xmin, ymin, xmax, ymax)
+        else:
+            isRobotInImage = False
+
+    # read all balls
+    for ball in label_data.balls:
+        xmin = str(ball.label.boundingBox.upperLeft.x)
+        ymin = str(ball.label.boundingBox.upperLeft.y)
+        xmax = str(ball.label.boundingBox.upperLeft.x)
+        ymax = str(ball.label.boundingBox.upperLeft.y)
+        if ball.label.boundingBox.upperLeft.x >= 0 and \
+                        ball.label.boundingBox.upperLeft.y >= 0 and \
+                        ball.label.boundingBox.upperLeft.x >= 0 and \
+                        ball.label.boundingBox.upperLeft.y >= 0:
+            isBallInImage = True
+            xml_object = ET.SubElement(xml_root, "object")
+            ET.SubElement(xml_object, "name").text = "ball"
+            create_xml_bndbox(xml_object, xmin, ymin, xmax, ymax)
+        else:
+            isBallInImage = False
+
+    if (isRobotInImage == True and isBallInImage == False) or \
+            (isRobotInImage == False and isBallInImage == True) or \
+            (isRobotInImage == True and isBallInImage == True):
+        tree = ET.ElementTree(xml_root)
+        if not os.path.exists(os.path.join(file_path, "annotations")):
+            os.makedirs(os.path.join(file_path, "annotations"))
+        xml_file_name = os.path.join(file_path, "annotations", file_name[:-4] + ".xml")
+        tree.write(xml_file_name)
+    else:
+        print("Skipped " + file_name[:-4] + ".xml, because of Robot:" + str(isRobotInImage) + " | Ball:" + str(isBallInImage))
+
+def create_xml_bndbox(xml_object, xmin, ymin, xmax, ymax):
+    # create bounding boxes
+    xml_bndbox = ET.SubElement(xml_object, "bndbox")
+    ET.SubElement(xml_bndbox, "xmin").text = xmin
+    ET.SubElement(xml_bndbox, "ymin").text = ymin
+    ET.SubElement(xml_bndbox, "xmax").text = xmax
+    ET.SubElement(xml_bndbox, "ymax").text = ymax
 
 def main():
     # parse arguments
@@ -16,6 +84,8 @@ def main():
     parser.add_argument('-d', '--destination', type=str, help='specify the output directory (absolute path)')
     parser.add_argument('-n', '--no-label-data', dest='label_data', action='store_false',
                         default=True, help='do not write the label data in protobuf chunk')
+    parser.add_argument('-x', '--no-xml-label-data', dest='xml_label', action='store_false',
+                        default=True, help='defines that the annotations are NOT saved also as *.xml files')
     parser.add_argument('imageset_id', type=int, help='the id of the imageset that is downloaded')
 
     args = parser.parse_args()
@@ -23,6 +93,7 @@ def main():
     imageset_id = args.imageset_id
     destination = args.destination if args.destination else '.'
     label_data = args.label_data
+    xml_label = args.xml_label
 
     # request handler
     requestHandler = RequestHandler()
@@ -61,6 +132,7 @@ def main():
         # do not write the labels data in protobuf chunk
         if not label_data:
             requestHandler.download_image(link, file_path, False)
+
         # write the labels in protobuf chunk
         else:
             # download image in file like object
@@ -103,6 +175,10 @@ def main():
             # write out the image incl protobuf
             write_label_chunk(file_path, image_data, encoded_protobuf, True)
 
+            # write xml annotations
+            if not xml_label:
+                save_annotations_to_xml(destination, name)
 
 if __name__ == '__main__':
     main()
+    sys.exit()
